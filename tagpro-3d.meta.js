58d393cb814e69e4b9e2d7473821db9d973e76aa
Kera <keratagpro@gmail.com>
2016-03-11 23:15:33 +0200

    Update to 0.0.3

--- tagpro-3d.meta.js
+++ tagpro-3d.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          TagPro 3D
 // @description   TagPro in 3D!
-// @version       0.0.2
+// @version       0.0.3
 // @author        Kera
 // @grant         GM_addStyle
 // @namespace     https://github.com/keratagpro/tagpro-3d/
@@ -12,5 +12,6 @@
 // @include       http://tangent.jukejuice.com*
 // @include       http://*.newcompte.fr*
 // @require       https://cdnjs.cloudflare.com/ajax/libs/three.js/r74/three.min.js
-// @require       https://keratagpro.github.io/tagpro-balls-3d/clipper.min.js
+// @require       https://keratagpro.github.io/tagpro-3d/clipper.min.js
+// @require       https://keratagpro.github.io/tagpro-3d/rgbquant.js
 // ==/UserScript==
